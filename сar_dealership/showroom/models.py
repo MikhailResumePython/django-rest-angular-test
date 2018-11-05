642b766246eff0db70f901692d56d818cf2dba09
mikedreyeu <mikedreyeu@gmail.com>
2018-11-05 11:32:53 +0300

    Add database

--- сar_dealership/showroom/models.py
+++ сar_dealership/showroom/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=64)
+
+class Car(models.Model):
+    price = models.FloatField()
+    power = models.IntegerField()
+    seats = models.IntegerField()
+    drive = models.CharField(max_length=64)
+    engine_type = models.CharField(max_length=64)
+    transmission = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='images')
+    models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+
